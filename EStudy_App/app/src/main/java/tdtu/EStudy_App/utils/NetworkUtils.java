1ece9b1ba6b06f1349a1f34a1dd0bf54f34aa468
Do Gia Huy <52200011@student.tdtu.edu.vn>
2024-10-27 23:49:44 +0700

    Connection Utils

--- EStudy_App/app/src/main/java/tdtu/EStudy_App/utils/NetworkUtils.java
+++ EStudy_App/app/src/main/java/tdtu/EStudy_App/utils/NetworkUtils.java
@@ -1,4 +1,16 @@
 package tdtu.EStudy_App.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 public class NetworkUtils {
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            return activeNetwork != null && activeNetwork.isConnected();
+        }
+        return false;
+    }
 }
