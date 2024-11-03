b9a5cf0e54db905cbe642fdab0511cf32b7072bf
thongabcxyz <thongcv12vn@gmail.com>
2024-11-03 20:35:08 +0700

    connect to realtime database firebase

--- EStudy_App/app/build.gradle.kts
+++ EStudy_App/app/build.gradle.kts
@@ -37,12 +37,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
