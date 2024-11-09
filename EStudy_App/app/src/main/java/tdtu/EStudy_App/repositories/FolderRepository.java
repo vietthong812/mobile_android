a3b77966b9a11bf802a9e120ff0f427f9d755dd4
Do Gia Huy <52200011@student.tdtu.edu.vn>
2024-11-09 23:56:10 +0700

    Repositories

---
+++ EStudy_App/app/src/main/java/tdtu/EStudy_App/repositories/FolderRepository.java
@@ -0,0 +1,38 @@
+package tdtu.EStudy_App.repositories;
+
+public class FolderRepository {
+    public FolderRepository() {
+    }
+
+    public boolean createFolder(String folderName) {
+        return true;
+    }
+
+    public boolean deleteFolder(String folderName) {
+        return true;
+    }
+
+    public boolean renameFolder(String folderName) {
+        return true;
+    }
+
+    public boolean moveFolder(String folderName) {
+        return true;
+    }
+
+    public boolean copyFolder(String folderName) {
+        return true;
+    }
+
+    public boolean shareFolder(String folderName) {
+        return true;
+    }
+
+    public boolean viewFolder(String folderName) {
+        return true;
+    }
+
+    public boolean searchFolder(String folderName) {
+        return true;
+    }
+}
